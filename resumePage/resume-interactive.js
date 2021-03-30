fea3939db41bcafe33e018ad8186db9ddaf1f144
mayrant <mayrant2004@yahoo.com>
2021-03-29 23:29:11 -0500

    animated name, changed disappearing image from CSS to JS, updated showNum function to more understandable code

--- resumePage/resume-interactive.js
+++ resumePage/resume-interactive.js
@@ -1,20 +1,20 @@
 // AT BARE MINIMUM:
 // 1. When clicked: add a fade in animation to the name on the resume.
+const animateName = () => {
+  document.getElementById('name').classList.add('animate__animated', 'animate__fadeInTopLeft');
+}
 
 // 2. Hide your phone number until it's clicked on.
-//    hide number with display:none in CSS Line 49
-var a;
-function showNum() {
-  if(a==1) {
-    document.getElementById("phoneNum").style.display="inline"
-    return a=0;
-  }
-  else {
-    document.getElementById("phoneNum").style.display="none"
-    return a=1;
-  }
+//    hidden number with display:none in CSS Line 49
+const showNum = () => {
+  document.getElementById('phoneNum').style.display='inline'
 }
 
 // 3. When a user hovers on the picture make it disappear.
-//    in CSS with #headshot:hover {opacity:0;} Line 90
+const hideImg = () => {
+  document.getElementById('headshot').style.display='none'
+}
 
+const showImg = () => {
+  document.getElementById('headshot').style.display='inline'
+}
